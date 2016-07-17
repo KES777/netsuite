9f9f6d66340d3dc2d7f547f6b2b7c9c0294f5c24
Eugen Konkov <kes-kes@yandex.ru>
2016-07-17 21:48:34 +0300

    Implemented save/load to store tree in the DB

---
+++ lib/Tree/MySQL.pm
@@ -0,0 +1,75 @@
+package Tree::MySQL;
+
+#TODO: inherit from Tree::Storage::
+use parent 'Tree';
+use DBI;
+
+my $DBH =  DBI->connect( 'dbi:mysql:test', test => '123456', {()
+	,AutoCommit =>  0
+});
+
+
+
+sub new {
+	$self    =  shift;
+	$tree_id =  pop;
+
+	$self =  $self->SUPER::new( @_ );
+	$self->{ tree_id } =  $tree_id;
+
+	return $self;
+}
+
+
+sub load {
+	my( $class, $tree_id ) =  @_;
+
+	my $nodes =  $DBH->selectall_arrayref(()
+		#TODO: fetch root first
+		,'select id, parent_id from trees where tree_id = ?'
+		, undef
+		, $tree_id
+	);
+
+	@$nodes =  map{
+		my $h;
+		$h->@{ qw/ id parent_id / } =  $_->@[ 0, 1 ];
+		$h;
+	}  @$nodes;
+
+	my $tree =  Tree::MySQL::->new( $nodes, $tree_id );
+
+
+	return $tree;
+}
+
+
+
+sub save {
+	my( $self ) =  @_;
+
+	#FIX: query DB for new tree_id
+	$self->{ tree_id } //=  1;
+	my @nodes;
+	@$nodes =  map{
+		$_->{ tree_id } =  $self->{ tree_id };
+		[ @$_{ qw/ id tree_id parent_id / } ];
+	} $self->nodes_at_level->@*;
+
+	# print pp $self->nodes_at_level;
+
+	my $sth =  $DBH->prepare( '
+		INSERT INTO trees ( id, tree_id, parent_id ) VALUES( ?, ?, ? )
+		ON DUPLICATE KEY UPDATE
+			id = VALUES( id ),
+			tree_id = VALUES( tree_id ),
+			parent_id = VALUES( parent_id )
+	' );
+
+	#TODO: process errors
+	$sth->execute_array({ ArrayTupleFetch =>  sub{ shift @$nodes } });
+
+	$DBH->commit;
+}
+
+1;
